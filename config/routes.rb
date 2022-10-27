3942f80b0b67d58c95523a154fc037e47c0b7cb9
Alessandra Piliero <alessandrapiliero92@gmail.com>
2022-10-27 15:02:58 +0100

    need to finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
   # resources :movies do
   #   resources 
   resources :lists, only: [:index, :new, :show, :create] do
-  resources :bookmarks
+  resources :bookmarks, only: [:new, :create]
+  resources :reviews, only: :create
   end 
   resources :bookmarks, only: [:destroy]
+  resources :reviews, only: :destroy
 end
